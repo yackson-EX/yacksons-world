ac8ee9412cbb36a98ab5bbcf8b534af2f76643dc
Yackson <reachme@yacksons.world>
2026-01-26 12:23:45 -0800

    Added dates to list view and headers for blog + review posts

---
+++ src/lib/dates.ts
@@ -0,0 +1,16 @@
+export const formatListDate = (date: Date) =>
+  new Intl.DateTimeFormat("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  })
+    .format(date)
+    .replace(/ /g, "-");
+
+export const formatFullDate = (date: Date) =>
+  new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date);
